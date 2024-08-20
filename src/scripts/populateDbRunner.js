8c23e8ed2fbe6e135307ce5286acd52118b520b8
Finn Langenkaemper <finn@webschuppen.com>
2024-08-20 14:54:21 +0200

    version 1.0

--- src/scripts/populateDbRunner.js
+++ src/scripts/populateDbRunner.js
@@ -1,28 +1,34 @@
-const { fetchYoutubeData, fetchTwitchData } = require('../utils/api-funcs.js')
-const { fetchRedditData } = require('../utils/api-funcs.js')
-const { creators } = require('../config/creators.js')
-const { supabase } = require('../lib/supabase.js')
+const { fetchYoutubeData, fetchTwitchData } = require("../utils/api-funcs.js");
+const { fetchRedditData } = require("../utils/api-funcs.js");
+const { supabase } = require("../lib/supabase.js");
 
 const setCreatorData = async () => {
-    console.log('executing cron job db-population-runner...' + new Date())    
+  console.log("executing cron job db-population-runner..." + new Date());
 
+  const { data, error } = await supabase.from("creators-data").select();
 
-    creators.map(async ( creator, index ) => {
-        const youtubeData = await fetchYoutubeData(creator.youtube);
-        const redditData = await fetchRedditData(creator.reddit)
-        const twitchData = await fetchTwitchData(creator.twitch)
+  if (data) {
+    console.log('Checking total of ' + data.length + ' creators');
+    console.log('--------------------------------------------------')
+    data.map(async (creator, index) => {
+      const youtubeData = await fetchYoutubeData(creator.youtube);
+      const redditData = await fetchRedditData(creator.reddit);
+      const twitchData = await fetchTwitchData(creator.twitch);
 
-        const { error } = await supabase
-            .from("creators")
-            .update({
-                creator: creator.creator,
-                youtube: youtubeData,
-                reddit: redditData,
-                twitch: twitchData,
-            })
-            .eq("id", index);
+      const { error } = await supabase
+        .from("creators")
+        .update({
+          youtube: youtubeData,
+          reddit: redditData,
+          twitch: twitchData,
+        })
+        .eq("creator", creator.creator);
 
-        if (error) console.log(error);
-    })}
-  
-module.exports = { setCreatorData }
+      if (error) console.log(error);
+    });
+  }
+  if (error) console.log(error);
+
+};
+
+module.exports = { setCreatorData };
