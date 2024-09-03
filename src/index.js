d29cb175e7e24db1f27687b7a6d5bbdb8f852f1a
Finn Langenkaemper <finn@webschuppen.com>
2024-09-03 09:19:09 +0200

    added independant cron functionality to execute fetches for each platform seperatly

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const axios = require('axios')
-const { setCreatorData } = require('./scripts/populateDbRunner.js')
+const { setYouTubeCreatorData, setRedditCreatorData, setTwitchCreatorData } = require('./scripts/populateDbRunner.js')
 const cron = require('node-cron')
 
 
 console.log('initial start')
 
-cron.schedule('0 5,11,17,23 * * *', () => {
-	setCreatorData();
+cron.schedule('*/30 * * * *', () => { //every 30 minutes
+	setRedditCreatorData();
 })
+
+cron.schedule('*/30 * * * *', () => { //every 30 minutes
+	setTwitchCreatorData();
+})
+
+
+cron.schedule('0 5,11,17,23 * * *', () => { //every 6 hours
+	setYouTubeCreatorData();
+})
+
