cbe22f82925a5d9c88503d4c0713e3037ee039e8
Finn Langenkaemper <finn@webschuppen.com>
2024-09-04 09:59:44 +0200

    instagram and tiktok integration

--- src/scripts/populateDbRunner.js
+++ src/scripts/populateDbRunner.js
@@ -1,5 +1,6 @@
 const { fetchYoutubeData, fetchTwitchData } = require("../utils/api-funcs.js");
 const { fetchRedditData } = require("../utils/api-funcs.js");
+const { fetchInstagramPost, fetchTikTokVideo } = require("../utils/apify-funcs.js")
 const { supabase } = require("../lib/supabase.js");
 
 const setYouTubeCreatorData = async () => {
@@ -15,6 +16,29 @@ const setYouTubeCreatorData = async () => {
         .from("creators")
         .update({
           youtube: youtubeData
+        })
+        .eq("creator", creator.creator);
+
+      if (error) console.log(error);
+    });
+  }
+  if (error) console.log(error);
+
+};
+
+const setInstagramCreatorData = async () => {
+  console.log("executing cron job Instagram data population at " + new Date());
+
+  const { data, error } = await supabase.from("creators-data").select();
+  if (data) {
+    console.log('Checking total of ' + data.length + ' creators');
+    console.log('--------------------------------------------------')
+    data.map(async (creator, index) => {
+      const instagramData = await fetchYoutubeData(creator.instagram);
+      const { error } = await supabase
+        .from("creators")
+        .update({
+          instagram: instagramData
         })
         .eq("creator", creator.creator);
 
@@ -76,4 +100,27 @@ const setRedditCreatorData = async () => {
 
 };
 
-module.exports = { setYouTubeCreatorData, setTwitchCreatorData, setRedditCreatorData };
+const setTikTokCreatorData = async () => {
+  console.log("executing cron job TikTok data population at " + new Date());
+
+  const { data, error } = await supabase.from("creators-data").select();
+  if (data) {
+    console.log('Checking total of ' + data.length + ' creators');
+    console.log('--------------------------------------------------')
+    data.map(async (creator, index) => {
+      const tiktokData = await fetchYoutubeData(creator.tiktok);
+      const { error } = await supabase
+        .from("creators")
+        .update({
+          tiktok: tiktokData
+        })
+        .eq("creator", creator.creator);
+
+      if (error) console.log(error);
+    });
+  }
+  if (error) console.log(error);
+
+};
+
+module.exports = { setYouTubeCreatorData, setTwitchCreatorData, setRedditCreatorData, setInstagramCreatorData, setTikTokCreatorData };
---
+++ src/utils/apify-funcs.js
@@ -0,0 +1,129 @@
+const Apify = require("apify");
+require("dotenv").config();
+
+async function fetchTikTokVideo(hashtag) {
+if(!hashtag) return null
+  const apifyClient = new Apify.ApifyClient({
+    token: process.env.APIFY_API_TOKEN,
+  });
+
+  const actorId = "clockworks/free-tiktok-scraper";
+  const input = {
+    excludePinnedPosts: false,
+    hashtags: [hashtag],
+    resultsPerPage: 1,
+    shouldDownloadCovers: false,
+    shouldDownloadSlideshowImages: false,
+    shouldDownloadSubtitles: false,
+    shouldDownloadVideos: false,
+  };
+
+  try {
+    const run = await apifyClient.actor(actorId).call(input);
+
+    const results = await apifyClient.dataset(run.defaultDatasetId).listItems();
+
+    const tiktokVideoData = {
+      title: results.items[0].text,
+      url: results.items[0].webVideoUrl,
+      author: results.items[0].authorMeta.name,
+      thumbnailUrl: results.items[0].videoMeta.coverUrl,
+      hashtahs: results.items[0].hashtags,
+    };
+
+    console.log("Successfull TikTok API call for: ", hashtag);
+    return tiktokVideoData;
+  } catch (error) {
+    console.log("Error running TikTok Video fetch for: " + hashtag, error);
+  }
+}
+
+async function fetchXPost(searchItem) {
+  const apifyClient = new Apify.ApifyClient({
+    token: process.env.APIFY_API_TOKEN,
+  });
+
+  const actorId = "apidojo/tweet-scraper";
+  const input = {
+    includeSearchTerms: true,
+    maxItems: 3,
+    onlyImage: false,
+    onlyQuote: false,
+    onlyTwitterBlue: false,
+    onlyVerifiedUsers: false,
+    onlyVideo: false,
+    searchTerms: [searchItem],
+    sort: "Top",
+    tweetLanguage: "en",
+  };
+
+  try {
+    const run = await apifyClient.actor(actorId).call(input);
+
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    const results = await apifyClient.dataset(run.defaultDatasetId).listItems();
+    console.log(results)
+
+    const xPostData = {
+      title: results.items[0].text,
+      url: results.items[0].url,
+      author: results.items[0].author.userName,
+      thumbnailUrl: results.items[0].extendedEntities.media_url_https,
+    };
+
+    console.log("Successfull X API call for: ", searchItem);
+    console.log(xPostData)
+    return xPostData;
+  } catch (error) {
+    console.log("Error running X Post fetch for: " + searchItem, error);
+  }
+}
+
+async function fetchInstagramPost(creator) {
+    if(!creator) return null
+
+    const apifyClient = new Apify.ApifyClient({
+      token: process.env.APIFY_API_TOKEN,
+    });
+  
+    const actorId = "apify/instagram-scraper";
+    const input = {
+        "addParentData": false,
+        "directUrls": [
+            `https://www.instagram.com/${creator}/`
+        ],
+        "enhanceUserSearchWithFacebookPage": false,
+        "isUserReelFeedURL": false,
+        "isUserTaggedFeedURL": false,
+        "resultsLimit": 1,
+        "resultsType": "posts",
+        "searchLimit": 1,
+        "searchType": "hashtag"
+    }
+  
+    try {
+      const run = await apifyClient.actor(actorId).call(input);
+  
+      const results = await apifyClient.dataset(run.defaultDatasetId).listItems();
+      console.log(results)
+  
+      const InstagramPostData = {
+        title: results.items[0].caption,
+        url: results.items[0].url,
+        author: results.items[0].author.ownerUsername,
+        thumbnailUrl: results.items[0].images,
+      };
+  
+      console.log("Successfull Instagram API call for: ", creator);
+      return InstagramPostData;
+    } catch (error) {
+      console.log("Error running Instagram Post fetch for: " + creator, error);
+    }
+  }
+
+module.exports = {
+  fetchTikTokVideo,
+  fetchXPost,
+  fetchInstagramPost
+};
